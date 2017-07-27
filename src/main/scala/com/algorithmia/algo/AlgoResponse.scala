88fd1e03d62f9713c1b6bb9a5a09c8e055ba2bf5
Kenny Daniel <kenny@algorithmia.com>
2017-07-26 17:49:10 -0700

    More useful error messages in algorithm exceptions

--- src/main/scala/com/algorithmia/algo/AlgoResponse.scala
+++ src/main/scala/com/algorithmia/algo/AlgoResponse.scala
@@ -43,19 +43,19 @@ case class AlgoSuccess(result: JsValue, metadata: Metadata, rawOutput: String) e
   override def asBytes: Array[Byte] = metadata.content_type match {
     case ContentTypeBinary => result match {
       case JsString(str) => Base64.decodeBase64(str) // Decode base 64
-      case _ => throw new IOException("Cannot cast algorithm response to byte array")
+      case _ => throw new IOException("Algorithm returned successfully, but cannot be converted to byte array")
     }
-    case _ => throw new IOException("Cannot cast algorithm response to byte array")
+    case _ => throw new IOException("Algorithm returned successfully, but cannot be converted to byte array")
   }
   override def map[T](f: AlgoSuccess => T): Option[T] = Some(f(this))
 }
 
 case class AlgoFailure(message: String, metadata: Metadata, rawOutput: String) extends AlgoResponse {
   override def isSuccess: Boolean = false
-  override def as[T](implicit reads: Reads[T]): T = throw new ClassCastException("Algorithm failure response cannot be cast")
+  override def as[T](implicit reads: Reads[T]): T = throw new ClassCastException("Algorithm returned an error, and cannot be converted to type")
   override def asOpt[T](implicit reads: Reads[T]): Option[T] = None
-  override def asString: String = throw new ClassCastException("Algorithm failure response cannot be cast")
-  override def asBytes: Array[Byte] = throw new ClassCastException("Algorithm failure response cannot be cast")
+  override def asString: String = throw new ClassCastException("Algorithm returned an error, and cannot be converted to  string")
+  override def asBytes: Array[Byte] = throw new ClassCastException("Algorithm returned an error, and cannot be converted to byte array")
   override def map[T](f: AlgoSuccess => T): Option[T] = None
 }
 
