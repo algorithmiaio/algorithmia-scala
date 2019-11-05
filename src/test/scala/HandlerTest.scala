e3104e7ff2138687ea84a16aed368ceb3d36e8af
James Sutton <james@algorithmia.com>
2019-11-05 19:07:35 -0400

    fixed specs2 tests

--- src/test/scala/HandlerTest.scala
+++ src/test/scala/HandlerTest.scala
@@ -32,9 +32,9 @@ class HandlerTest extends Specification with BeforeEach with AfterEach {
   }
 
   def execute[I, O](
-      input: String,
-      algo: AbstractAlgorithm[I, O]
-  )(implicit r: Reads[I], w: Writes[O], i: ClassTag[I]): JsValue = {
+                     input: String,
+                     algo: AbstractAlgorithm[I, O]
+                   )(implicit r: Reads[I], w: Writes[O], i: ClassTag[I]): JsValue = {
     System.setIn(new ByteArrayInputStream(input.getBytes(java.nio.charset.StandardCharsets.UTF_8.name)))
     val handler = Algorithmia.handler(algo)
     handler.serve()
@@ -58,63 +58,65 @@ class HandlerTest extends Specification with BeforeEach with AfterEach {
     output.mkString("\n")
   }
 
-  "With String primitive I/O, no loading" should {
-    val input = """{"content_type":"text","data":"algorithmia"}"""
-    val expected = Json.parse("""{"metadata":{"content_type":"text"},"result":"hello algorithmia"}""")
-    val output = execute(input, new Algorithm1)
-    output mustEqual expected
-  }
+  "algorithm execution" should {
+    "With successful primitive string IO" in {
+      val input = """{"content_type":"text","data":"algorithmia"}"""
+      val expected = Json.parse("""{"metadata":{"content_type":"text"},"result":"hello algorithmia"}""")
+      val output = execute(input, new Algorithm1)
+      output mustEqual expected
+    }
 
-  "With Custom case class I/O, with loading" should {
-    val input = """{"content_type":"json","data":{"foo":"algorithmia"}}"""
-    val expected = Json.parse("""{"metadata":{"content_type":"json"},"result":{"bar":"hello algorithmia","num":25}}""")
-    val output = execute(input, new Algorithm2)
-    output mustEqual expected
-  }
+    "With Custom case class I/O, with loading" in {
+      val input = """{"content_type":"json","data":{"foo":"algorithmia"}}"""
+      val expected = Json.parse("""{"metadata":{"content_type":"json"},"result":{"bar":"hello algorithmia","num":25}}""")
+      val output = execute(input, new Algorithm2)
+      output mustEqual expected
+    }
 
-  "With failure during loading" should {
-    val input = """{"content_type":"text","data":"algorithmia"}"""
-    val expected = Json.parse(
-      """{"error":{"message":"Exception: This is an expected failure state","error_type":"class java.lang.Exception"}}"""
-    )
-    val output: JsValue = execute(input, new Algorithm3)
-    val stacktrace = output \ "error" \ "stacktrace"
-    val message = output \ "error" \ "message"
-    val error_type = output \ "error" \ "error_type"
-    stacktrace.get.toString() must contain("java.lang.Exception: This is an expected failure state")
-    message mustEqual expected \ "error" \ "message"
-    error_type mustEqual expected \ "error" \ "error_type"
-  }
+    "With failure during loading" in {
+      val input = """{"content_type":"text","data":"algorithmia"}"""
+      val expected = Json.parse(
+        """{"error":{"message":"Exception: This is an expected failure state","error_type":"class java.lang.Exception"}}"""
+      )
+      val output: JsValue = execute(input, new Algorithm3)
+      val stacktrace = output \ "error" \ "stacktrace"
+      val message = output \ "error" \ "message"
+      val error_type = output \ "error" \ "error_type"
+      stacktrace.get.toString() must contain("java.lang.Exception: This is an expected failure state")
+      message mustEqual expected \ "error" \ "message"
+      error_type mustEqual expected \ "error" \ "error_type"
+    }
 
-  "With failure during runtime, with stdout loader" should {
-    val input = """{"content_type":"text","data":"algorithmia"}"""
-    val expected = Json.parse(
-      """{"error":{"message":"Exception: This is another expected failure state","error_type":"class java.lang.Exception"}}"""
-    )
-    val output: JsValue = execute(input, new Algorithm4)
-    val stacktrace = output \ "error" \ "stacktrace"
-    val message = output \ "error" \ "message"
-    val error_type = output \ "error" \ "error_type"
-    stacktrace.get.toString() must contain("java.lang.Exception: This is another expected failure state")
-    message mustEqual expected \ "error" \ "message"
-    error_type mustEqual expected \ "error" \ "error_type"
-  }
+    "With failure during runtime, with stdout loader" in {
+      val input = """{"content_type":"text","data":"algorithmia"}"""
+      val expected = Json.parse(
+        """{"error":{"message":"Exception: This is another expected failure state","error_type":"class java.lang.Exception"}}"""
+      )
+      val output: JsValue = execute(input, new Algorithm4)
+      val stacktrace = output \ "error" \ "stacktrace"
+      val message = output \ "error" \ "message"
+      val error_type = output \ "error" \ "error_type"
+      stacktrace.get.toString() must contain("java.lang.Exception: This is another expected failure state")
+      message mustEqual expected \ "error" \ "message"
+      error_type mustEqual expected \ "error" \ "error_type"
+    }
 
-  //Not necessary to define a Reads and Write for List[_] when _ is primitive, but it still gets flagged by the IDE as an error
-  "With list type I/O" should {
-    val input = """{"content_type":"json","data":["five", "four", "three"]}"""
-    val expected = Json.parse("""{"metadata":{"content_type":"json"},"result":[4, 4, 5]}""")
-    val output = execute(input, new Algorithm5)
-    output mustEqual expected
-  }
+    //Not necessary to define a Reads and Write for List[_] when _ is primitive, but it still gets flagged by the IDE as an error
+    "With list type I/O" in {
+      val input = """{"content_type":"json","data":["five", "four", "three"]}"""
+      val expected = Json.parse("""{"metadata":{"content_type":"json"},"result":[4, 4, 5]}""")
+      val output = execute(input, new Algorithm5)
+      output mustEqual expected
+    }
 
-  "With multiple basic requests" should {
-    val input = """{"content_type":"text","data":"algorithmia"}"""
-    val expected = """{"metadata":{"content_type":"text"},"result":"hello algorithmia"}"""
-    val dupedInput: String = duplicateRequest(input, 10)
-    val dupedExpected: JsValue = Json.toJson(duplicateRequest(expected, 10).split("\n"))
-    val outputs: JsValue = execute(dupedInput, new Algorithm1)
-    outputs mustEqual dupedExpected
-  }
+    "With multiple basic requests" in {
+      val input = """{"content_type":"text","data":"algorithmia"}"""
+      val expected = """{"metadata":{"content_type":"text"},"result":"hello algorithmia"}"""
+      val dupedInput: String = duplicateRequest(input, 10)
+      val dupedExpected: JsValue = Json.toJson(duplicateRequest(expected, 10).split("\n"))
+      val outputs: JsValue = execute(dupedInput, new Algorithm1)
+      outputs mustEqual dupedExpected
+    }
 
+  }
 }
