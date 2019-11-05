d7d5d1eee3af0a07cac8f64a2d43677870f375f3
James Sutton <james@algorithmia.com>
2019-11-05 18:30:13 -0400

    ran scalafmt on requestHandler

--- src/main/scala/com/algorithmia/handler/RequestHandler.scala
+++ src/main/scala/com/algorithmia/handler/RequestHandler.scala
@@ -9,10 +9,11 @@ case class RequestHandler[I]() {
 
   def processRequest(line: String)(implicit s: Reads[I], obj: ClassTag[I]): Try[I] = {
     (Json.parse(line) \ "data") match {
-      case JsDefined(value) => Json.fromJson[I](value) match {
-        case JsSuccess(value, _) => Success(value)
-        case JsError(_) =>Failure(new Exception(s"Unable to convert ${line} into ${obj}"))
-      }
+      case JsDefined(value) =>
+        Json.fromJson[I](value) match {
+          case JsSuccess(value, _) => Success(value)
+          case JsError(_) => Failure(new Exception(s"Unable to convert ${line} into ${obj}"))
+        }
       case JsUndefined() => Failure(new Exception(s"unable to find key 'data' in request json object:\n${line}"))
     }
   }
