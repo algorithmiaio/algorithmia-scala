4095dc2c1826e115692ada9b91e24130b8f7a484
jakemuntarsi <jake.munta@capabilitysource.com>
2020-10-26 11:53:40 -0400

    DEV-77 changed yml file

--- build.sbt
+++ build.sbt
@@ -14,11 +14,12 @@ libraryDependencies ++= Seq(
   "org.apache.commons" % "commons-lang3" % "3.9",
   "com.google.code.gson" % "gson" % "2.8.6",
   // Test
+  "org.scalatest" %% "scalatest" % "3.2.2" % Test,
   "commons-io" % "commons-io" % "2.6" % Test,
   "junit" % "junit" % "4.12" % Test,
   // "org.specs2" %% "specs2" % "3.7" % Test
   "com.typesafe.play" %% "play-specs2" % "2.7.3" % Test,
-  "org.scalatest" %% "scalatest" % "3.2.2" % Test,
+
 )
 
 // Build for multiple scala versions
--- src/test/scala/AlgorithmiaInsightsSpec.scala
+++ src/test/scala/AlgorithmiaInsightsSpec.scala
@@ -1,30 +1,31 @@
 import AlgorithmTest.key
 import com.algorithmia.Algorithmia
 import com.algorithmia.algo.AlgorithmiaInsights
-import org.scalatest.{FlatSpec, MustMatchers}
+
 import org.specs2.mutable._
 
-object AlgorithmiaInsightsSpec extends FlatSpec with MustMatchers {
+object AlgorithmiaInsightsSpec extends Specification  {
  val keys: String = System.getenv("ALGORITHMIA_API_KEY")
  val client = Algorithmia.client(keys)
  val algo = new AlgorithmiaInsights(client, "")
- "api key" should
-   "define environment variable ALGORITHMIA_API_KEY" in {
-  key must not be null
+ "api key" should {
+  "define environment variable ALGORITHMIA_API_KEY" in {
+   keys must not be null
+  }
  }
 
 
- "AlgorithmiaInsight Calling status" should "be false " in {
-   algo.report_insight().isSuccess must be
-  false
-
+ "AlgorithmiaInsight Calling status" should {"be false " in {
+  algo.report_insight().isSuccess must be
+    false
+ }
  }
 val algo2 = new AlgorithmiaInsights(client, """{"cats_in_image": 4, "dogs_in_image": 7}""")
- "Algorithmia Client" should "give  status" in {
+ "Algorithmia Client" should  {"give  status" in {
 
   algo2.report_insight().isSuccess must be
   true
 
-
+ }
  }
 }
